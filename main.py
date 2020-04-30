aa4853ca24a9ae3c341a6905aa0fe65454a71706
BlueMeringue <renzbrian.cruz@hotmail.com>
2020-04-29 22:03:39 -0600

    Chatbot on discord works

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
 from identifier import MaidChan # The bot's identity
+from ChatBot import *
 
 client = discord.Client()
+
 myBot = MaidChan()
 
 @client.event
@@ -9,5 +11,10 @@ async def on_ready():
     print("The bot is ready!")
     await client.change_presence(activity=discord.Game(name="Serving"))
 
+@client.event
+async def on_message(message):
+    if message.content.startswith('!'):
+        response = chatbot_response(message.content)
+        await message.channel.send(response)
 
 client.run(myBot.TOKEN)
